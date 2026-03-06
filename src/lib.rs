67eb372f98319c31be06c340ecef0dc7170f2d32
SimonThormeyer <simon.thormeyer@wire.com>
2026-03-06 18:01:01 +0100

    chore: make test function async

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,18 @@
 #![deny(clippy::all)]
 
+use core_crypto::ConnectionType;
+use core_crypto::CoreCrypto;
+use core_crypto::Database;
+use core_crypto::DatabaseKey;
 use napi_derive::napi;
 
 #[napi]
-pub fn plus_100(input: u32) -> u32 {
+pub async fn plus_100(input: u32) -> u32 {
+  let key = DatabaseKey::generate();
+  let db = Database::open(ConnectionType::Persistent("test-db"),&key).await.unwrap();
+  let cc = CoreCrypto::new(db);
+  let ctx = cc.new_transaction().await.unwrap();
+  ctx.set_data(b"hello napi".into()).await.unwrap();
+  ctx.finish().await.unwrap();
   input + 100
 }
